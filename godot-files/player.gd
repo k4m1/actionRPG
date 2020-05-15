0b23f8005411e26aedb3f16dd1b8206b23b1372f
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-15 17:35:50 -0400

    added smoothe movement and/via deltas

--- godot-files/player.gd
+++ godot-files/player.gd
@@ -1,19 +1,24 @@
 extends KinematicBody2D
 
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const FRICTION = 100
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 10
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -10
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -10
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 10
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector - input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	
 		
 	move_and_collide(velocity)
 		
