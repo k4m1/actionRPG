2cef8cf2155c07d837e59da95d34afee0212e1a9
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-18 00:58:50 -0400

    added 8 directional animations and tighented player movement feel

--- godot-files/player.gd
+++ godot-files/player.gd
@@ -1,13 +1,14 @@
 extends KinematicBody2D
 
 const ACCELERATION = 500
-const MAX_SPEED = 100
-const FRICTION = 500
+const MAX_SPEED = 90
+const FRICTION = 600
 
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get('parameters/playback')
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -18,8 +19,10 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set('parameters/Idle/blend_position', input_vector)
 		animationTree.set('parameters/Run/blend_position', input_vector)
+		animationState.travel('Run')
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel('Idle')
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	
