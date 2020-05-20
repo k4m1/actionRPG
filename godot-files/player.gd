e81187a43b7c3429b8b83452bb7c79e1011e618c
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-20 13:45:56 -0400

    fixed black screen bug refactored _phys_process

--- godot-files/player.gd
+++ godot-files/player.gd
@@ -23,7 +23,7 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
-	match state:
+		match state:
 			MOVE:
 				move_state(delta)
 			
@@ -34,7 +34,7 @@ func _physics_process(delta):
 				attack_state(delta)
 
 
-func move_state():
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -56,7 +56,7 @@ func move_state():
 	
 	
 func attack_state(delta):
-	pass
+	animationPlayer.play("AttackRight")
 	
 		
 
